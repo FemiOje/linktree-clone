f5005f41c8af2f33b0f6c3d79ff27ae616028c12
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-10-29 21:36:37 +0100

    profile section

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';  
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+        } from "react-router-dom";
 import './index.css';
 import App from './App';
+import NoMatch from './NoMatch.jsx'
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NoMatch />
+  },
+  {
+        path: "*",
+        element: <NoMatch />,
+        errorElement: <NoMatch />,
+  },
+    ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
