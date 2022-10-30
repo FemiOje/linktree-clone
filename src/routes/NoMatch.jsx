e76cd3b888faeab95da193d02b4a8931fdde4216
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-10-30 18:26:32 +0100

    fix: typos

--- src/routes/NoMatch.jsx
+++ src/routes/NoMatch.jsx
@@ -18,7 +18,7 @@ export default function NoMatch() {
         <Link to='..' relative='path' className='go-back'>
           <span className='go-back-arrow'>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="#344054" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="#344054" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </span>
           Go back
