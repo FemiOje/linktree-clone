f3d7a1e62ebaf412bbb97c6e3a2e63a9104f51e8
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-11-05 17:48:01 +0100

    fix: syntax error(element={<Contact/>})

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Router>
           <Routes>
               <Route path='/' element={<Homepage />} />
-              <Route path='/contact' element={Contact} />
+              <Route path='/contact' element={<Contact />} />
               <Route path='*' element={<NoMatch />} />
           </Routes>
         </Router>
