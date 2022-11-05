7ea50644715b71f3bec831125991dea5a1a24431
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-11-05 17:44:11 +0100

    add: Contact compoonent and Contact route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Homepage from "./routes/Homepage";
 import NoMatch from "./routes/NoMatch";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
         <Router>
           <Routes>
               <Route path='/' element={<Homepage />} />
-              <Route path="*" element={<NoMatch />} />
+              <Route path='/contact' element={Contact} />
+              <Route path='*' element={<NoMatch />} />
           </Routes>
         </Router>
     </>
